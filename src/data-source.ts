b08c2b6515ca25949c5b1ecf1c634de637d8b3e0
zlfera <zlfera@gmail>
2023-05-28 14:12:28 +0800

    change xu

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'xin',
+  database: 'xu',
   entities: [WuLiao],
   migrations: ['./migrations/*.ts'],
 });
